6a4b64119af8bbc0aa1f73ceee81c9d0b79a995d
RohitKonge <rohitkonge08@gmail.com>
2025-04-23 11:06:36 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,15 +19,6 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <meta name="google-site-verification" content="29_3z6RLHHthRxoN5dUVYuHPtrmiV36cEmXty1LmqS0" />
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-N2W53PV8BD"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-N2W53PV8BD');
-        </script>
       </head>
       <body className="bg-gray-100 min-h-screen">
         <main className="container mx-auto px-4 py-10">
